3b35e69fbda45053b9e17f1287febe19a7e4e0ff
aayushnagargujjar <aayushnagargujjar@gmail.com>
2025-05-26 22:40:31 +0530

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -1,10 +1,11 @@
 import os
 import json
+import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 
 # Initialize Firebase
 def init_firebase():
-    if not firestore._apps:
+    if not firebase_admin._apps:
         firebase_config_json = os.environ.get("FIREBASE_KEY_JSON")
         if firebase_config_json:
             cred = credentials.Certificate(json.loads(firebase_config_json))
@@ -12,3 +13,4 @@ def init_firebase():
             cred = credentials.Certificate("firebase-key.json")
         initialize_app(cred)
     return firestore.client()
+
